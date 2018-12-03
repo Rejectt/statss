bdc052b10495fe43d3849094860de07b73365972
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-03 09:04:52 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on('ready', function() {
 
 
 
-const developers = ["517719569666932737","",""]
+const developers = ["518012022500229122","",""]
 const adminprefix = "!";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
